a01f038e215b88645a7f2c9ad798552fbfa38823
Adam Oneil <adamsoftware@gmail.com>
2021-05-12 17:04:48 -0400

    progress on BuildZipFile

--- QueuedJobs.Functions/BuildZipFile.cs
+++ QueuedJobs.Functions/BuildZipFile.cs
@@ -1,6 +1,4 @@
-using Dapper.CX.SqlServer.Extensions.Int;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using Notification.Shared;
 using Notification.Shared.Models;
@@ -8,7 +6,6 @@ using Notification.Shared.Requests;
 using Notification.Shared.Responses;
 using QueuedJobs.Abstract;
 using QueuedJobs.Functions.Extensions;
-using QueuedJobs.Library.Interfaces;
 using QueuedJobs.Models;
 using System.Threading.Tasks;
 
@@ -17,23 +14,28 @@ namespace QueuedJobs.Functions
     public static class BuildZipFile
     {
         [FunctionName("BuildZipFile")]
-        public static void Run([QueueTrigger("zip-builder", Connection = "ConnectionString")]string data, 
+        public static void Run(
+            [QueueTrigger("zip-builder", Connection = "ConnectionString")]string data, 
             ILogger log, ExecutionContext context)
         {
-            int id = int.Parse(data);
+            int id = int.Parse(data); // int because JobTracker is QueuedJob<int>
             var config = context.GetConfig();
-            new ZipFileBuilder(
-                config["ConnectionStrings:Storage"],
-                config["ConnectionStrings:Database"], 
-                log).ExecuteAsync(id).Wait();
-        }
 
+            var databaseConnection = config["ConnectionStrings:Database"];
+            var repo = new JobTrackerRepository(databaseConnection);
+
+            var storageConnection = config["ConnectionStrings:Storage"];
+
+            new ZipFileBuilder(storageConnection, repo, log)
+                .ExecuteAsync(id)
+                .Wait();
+        }
 
         public class ZipFileBuilder : JobRunner<JobTracker, int, ZipRequest, ZipResult>
         {
             private readonly string _storageConnection;
 
-            public ZipFileBuilder(string storageConnection, string databaseConnection, ILogger logger) : base(new JobTrackerRepository(databaseConnection), logger)
+            public ZipFileBuilder(string storageConnection, JobTrackerRepository repository, ILogger logger) : base(repository, logger)
             {
                 _storageConnection = storageConnection;
             }
