5ae22a6dbac40951e59ebda5a5a4b299356db861
Adam Oneil <adamsoftware@gmail.com>
2021-05-23 10:53:48 -0400

    v1.0.4 added QueueClientHelperBase

--- Notification.Demo/Services/QueueManager.cs
+++ Notification.Demo/Services/QueueManager.cs
@@ -1,35 +1,17 @@
 ﻿using Azure.Storage.Queues;
 using Notification.Shared.Models;
-using System.Collections.Generic;
+using QueuedJobs.Abstract;
 
 namespace Notification.Demo.Services
 {
     /// <summary>
     /// provides central access to all queues in the application that share a connection string
     /// </summary>
-    public class QueueManager
+    public class QueueManager : QueueClientHelperBase
     {
-        private readonly string _storageConnection;
-        private readonly Dictionary<string, QueueClient> _queues;        
-
-        public QueueManager(string connectionString)
-        {
-            _storageConnection = connectionString;
-            _queues = new Dictionary<string, QueueClient>();            
-        }
-
-        public QueueClient this[string queueName]
-        {
-            get
-            {
-                if (!_queues.ContainsKey(queueName))
-                {
-                    _queues.Add(queueName, new QueueClient(_storageConnection, queueName));
-                }
-
-                return _queues[queueName];
-            }
-        }
+        public QueueManager(string connectionString) : base(connectionString)
+        {                
+        }     
 
         public QueueClient ZipBuilder => this[JobTracker.ZipBuilderQueue];
     }
---
+++ QueuedJobs.Library/Abstract/QueueClientHelperBase.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Storage.Queues;
+using System.Collections.Generic;
+
+namespace QueuedJobs.Abstract
+{
+    /// <summary>
+    /// provides central access to all queues in the application that share a connection string.
+    /// Extend with your own QueueClient objects that wrap this[string]
+    /// </summary>
+    public abstract class QueueClientHelperBase
+    {
+        private readonly string _storageConnection;
+        private readonly Dictionary<string, QueueClient> _queues;
+
+        public QueueClientHelperBase(string connectionString)
+        {
+            _storageConnection = connectionString;
+            _queues = new Dictionary<string, QueueClient>();
+        }
+
+        public QueueClient this[string queueName]
+        {
+            get
+            {
+                if (!_queues.ContainsKey(queueName))
+                {
+                    _queues.Add(queueName, new QueueClient(_storageConnection, queueName));
+                }
+
+                return _queues[queueName];
+            }
+        }
+    }
+}
